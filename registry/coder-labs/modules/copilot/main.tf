162dab54f683f6a50223b4bd8bfb45bc41ac9a1f
Michael Orlov <michael.orlov@gmail.com>
2025-11-21 14:19:22 -0500

    fix: CDES-216 Create fully qualified markdown for final_system_prompt

--- registry/coder-labs/modules/copilot/main.tf
+++ registry/coder-labs/modules/copilot/main.tf
@@ -221,10 +221,7 @@ additional user input
 details, or encounter blockers
   EOT
 
-  final_system_prompt = <<-EOT
-  ${var.system_prompt}
-  ${local.task_reporting_prompt2}
-  EOT
+  final_system_prompt = "${var.system_prompt}\n\n${local.task_reporting_prompt2}"
 
   # final_system_prompt = "<system>\n${var.system_prompt}\n</system>"
   # final_task_prompt = replace(local.task_reporting_prompt2, "/[\r\n]/", "")
